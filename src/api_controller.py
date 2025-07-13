37b580cf9d088100e7067a9b1fae3a0a19c43e39
Francesco <francesco2822003@gmail.com>
2025-07-13 16:28:20 +0200

    Fixed Flask setting for incoming connections

--- src/api_controller.py
+++ src/api_controller.py
@@ -22,4 +22,4 @@ def run_tremor_analysis():
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
